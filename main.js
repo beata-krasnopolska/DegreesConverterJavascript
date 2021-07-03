e12c0257e5bb9c5ffc243491e26a499f2a071b19
beata.krasnopolska@gmail.com <beata.krasnopolska@gmail.com>
2021-07-04 01:08:14 +0200

    no message

---
+++ main.js
@@ -0,0 +1,53 @@
+const one = document.querySelector('.one');
+const two =document.querySelector('.two');
+const converter = document.querySelector('#converter');
+const result = document.querySelector('.result');
+const convBtn =document.querySelector('.conv');
+const resetBtn =document.querySelector('.reset');
+const changeBtn =document.querySelector('.change');
+let fahrenheit;
+let celsius;
+
+const swap = () => {
+    if(one.innerText === "°C"){
+        one.innerText = "°F";
+        two.innerText = '°C';
+        result.innerText = '';
+    }
+    else{
+        one.innerText = "°C";
+        two.innerText = '°F';
+        result.innerText = '';
+    }
+};
+
+    const celToFahr = () =>{
+        fahrenheit = converter.value *1.8 +32;
+        result.innerText = `${converter.value} °C to ${fahrenheit.toFixed(1)} °F`;
+        converter.value = '';
+    };
+        const fahrToCel = () =>{
+        celsius = (converter.value - 32) / 1.8;
+        result.innerText = `${converter.value} °C to ${celsius.toFixed(1)} °F`;
+        converter.value = '';
+    };
+const conversion = () => {
+    if(converter.value !== ''){
+        if(one.value === '°C'){
+            celToFahr();
+        }else{
+            fahrToCel();
+        }
+    }
+    else{
+        result.innerText = 'Musisz wpisać wartość';
+    }
+};
+const reset = () => {
+    converter.value = '';
+    result.innerText = '';
+};
+
+convBtn.addEventListener('click', conversion);
+changeBtn.addEventListener('click', swap);
+resetBtn.addEventListener('click', reset);
